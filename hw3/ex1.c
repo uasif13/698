75612bf6f8c40ed70b78b4b2cbe26941ef085789
Asif Uddin <uasif13@gmail.com>
2025-02-23 10:42:28 -0500

    window create

--- hw3/ex1.c
+++ hw3/ex1.c
@@ -17,11 +17,27 @@ int main(int argc, char* argv[])
 
     // Get my rank
     int comm_size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &comm_size);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
 
-    printf("Process %d has initialised its MPI environment.\n", comm_size);
+    printf("%d Processes in  MPI environment.\n", comm_size);
 
+    int my_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
+
+    printf("Process %d has initialised its MPI environment.\n", my_rank);
     // Tell MPI to shut down.
+
+    const int ARRAY_SIZE = 2;
+    double window_buffer[ARRAY_SIZE];
+    MPI_Win window;
+
+    if (my_rank == 1) {
+    	window_buffer[1] = 3.5;
+    }
+    MPI_Win_create(window_buffer, ARRAY_SIZE * sizeof(int),sizeof(int), MPI_INFO_NULL, MPI_COMM_WORLD, &window);
+
+    MPI_Win_fence(0,window);
+
     MPI_Finalize();
 
     return EXIT_SUCCESS;
